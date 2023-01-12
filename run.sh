f26f4ee43b2affded32c898cdb47ea30a5b214f4
melvin <melvin.tsui@airmconsulting.com>
2023-01-12 15:01:08 -0600

    <feat> remove container afterwards to save space

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 docker run --rm --privileged multiarch/qemu-user-static --reset -p yes 
 
-docker run -it --platform linux/arm64/v8 --privileged -v $1:/workspace tegra-ubuntu
+docker run -it --rm --platform linux/arm64/v8 --privileged -v $1:/workspace tegra-ubuntu
